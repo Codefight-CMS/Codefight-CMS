a2b68a6721dec1eb024b7b55835d1f30c154dd73
Dbashyal <dbashyal@gmail.com>
2012-05-15 23:33:19 +1000

    added option to add dynamic tag links {{tag url='' title='' rel=''}}

--- codefight/app/libraries/Cf_process_lib.php
+++ codefight/app/libraries/Cf_process_lib.php
@@ -11,10 +11,59 @@ class Cf_process_lib
     {
         $CI =& get_instance();
 
-        $html_string = preg_replace('/\[\[BASE_URL\]\]/i', base_url(), $html_string);
-        $html_string = preg_replace('/\[\*\[/i', '[[', $html_string);
-        $html_string = preg_replace('/\]\*\]/i', ']]', $html_string);
+        if($CI->cfModule != 'admin'){
+            $html_string = preg_replace('/\[\[BASE_URL\]\]/i', base_url(), $html_string);
+            $html_string = preg_replace('/\[\*\[/i', '[[', $html_string);
+            $html_string = preg_replace('/\]\*\]/i', ']]', $html_string);
 
+            preg_match_all('/{{tag\s\s*(.*)\s*}}/isU', $html_string, $matches);
+            if(isset($matches[1])){
+                foreach($matches[1] as $k => $v){
+                    preg_match_all("@\s*(.+)='(.+)'@isU", trim($v), $v);
+                    if(isset($v[1])){
+                        $tagCode = $matches[0][$k];
+                        $title = $url = $attr = '';
+                        foreach($v[1] as $k1 => $v1){
+                            $v1 = strtolower(trim($v1));
+                            $v[2][$k1] = trim($v[2][$k1]);
+                            if($v1=='url'){
+                                if(!preg_match('/mailto:|https?:\/\//isU',$v[2][$k1])){
+                                    $v[2][$k1] = site_url(url_title(strtolower(trim($v[2][$k1]))));
+                                }
+                                $url = $v[2][$k1];
+                            } else if($v1=='title'){
+                                $title = $v[2][$k1];
+                            } else {
+                                $attr .= " {$v1}=\"{$v[2][$k1]}\"";
+                            }
+                        }
+                        if(empty($url) && !empty($title)){
+                            $url = site_url('blog/tag/'.url_title(strtolower($title)));
+                        }
+                        $tagReplace = '<a href="'. $url .'"'. $attr .'>' . $title . '</a>';
+                        $html_string = str_replace($tagCode, $tagReplace, $html_string);
+                    }
+                }
+            }
+            //preg_match_all('/{{tag\s*(url=\'(.*)\')?\s*(rel=\'(.*)\')?\s*(title=\'(.*)\')?\s*(.*)}}/iSu', $html_string, $matches);
+        }
+/*
+Array
+(
+    [0] =&gt; Array
+        (
+            [0] =&gt; {{tag url='http://codefight.org/' rel='external,nofollow' title='Codefight CMS'}}
+            [1] =&gt; {{tag title='Codeigniter CMS'}}
+        )
+
+    [1] =&gt; Array
+        (
+            [0] =&gt; url='http://codefight.org/' rel='external,nofollow' title='Codefight CMS'
+            [1] =&gt; title='Codeigniter CMS'
+        )
+
+)
+ */
         //Do you want to minify your html
         $html_tidy = Library('asset')->minify_html; //$CI->config->item('cf_minify_html');
 
