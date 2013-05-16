a4bfa58fdef6bb843148c48b77f39322b11d4d62
damu <dbashyal@gmail.com>
2013-05-16 21:56:58 +1000

    updated google analytics code

--- app/views/frontend/templates/core/blocks/seo/google_analytics.php
+++ app/views/frontend/templates/core/blocks/seo/google_analytics.php
@@ -1,14 +1,15 @@
 <?php if (!defined('BASEPATH')) exit(__('No direct script access allowed')); ?>
 <script type="text/javascript">
+
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', '<?php echo $this->setting->google_analytics_id ?>']);
+    //_gaq.push(['_setDomainName', 'codefight.org']); @todo::make it dynamic
     _gaq.push(['_trackPageview']);
+
     (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
+
 </script>
