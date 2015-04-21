cf20cab27a21cb872d4ec8703f21149567e43f9b
damu <damodar.bashyal@dicksmith.com.au>
2015-04-21 19:13:05 +1000

    upgrading CF to CI 3.0 release branch

--- trunk/app/config/database.php
+++ trunk/app/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "codefight";
-$db['default']['password'] = "cms";
+$db['default']['username'] = "root";
+$db['default']['password'] = "password";
 $db['default']['database'] = "codefight";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "cf_";
