218d9db9bcec4f1fd939e490389d8a2d80338ad4
dbashyal <dbashyal@gmail.com>
2016-07-07 22:38:36 +1000

    updated router to use lowercase routes.

--- trunk/app/core/MY_Router.php
+++ trunk/app/core/MY_Router.php
@@ -176,117 +176,6 @@ class MY_Router extends CI_Router
         log_message('debug', 'No URI present. Default controller set.');
     }
 
-
-    // --------------------------------------------------------------------
-
-    /**
-     * Validates the supplied segments.  Attempts to determine the path to
-     * the controller.
-     *
-     * @access    private
-     * @param    array
-     * @return    array
-     */
-    /*function _validate_request($segments)
-   	{
-   		if (count($segments) == 0)
-   		{
-   			return $segments;
-   		}
-
-   		// Does the requested controller exist in the root folder?
-   		if (file_exists(APPPATH.'controllers/'.$segments[0] . '.php'))
-   		{
-   			return $segments;
-   		}
-
-   		// Is the controller in a sub-folder?
-   		if (is_dir(APPPATH.'controllers/'.$segments[0]))
-   		{
-   			// Set the directory and remove it from the segment array
-   			$this->set_directory($segments[0]);
-   			$segments = array_slice($segments, 1);
-
-               /*
-                   * Updated for codefight cms
-                   * Added new code to allow multi-level sub-folder
-                   *//*
-               $subfolder = false;
-               if (((count($segments) > 0) && is_dir(APPPATH . 'controllers/' . $this->directory . $segments[0])) && (!file_exists(APPPATH . 'controllers/' . $this->directory . $segments[0] . '.php'))) $subfolder = true;
-
-               while ($subfolder)
-               {
-                   if (!isset($segments[0])) break;
-
-                   //Set the directory and remove it from the segment array
-                   $this->set_directory($this->directory . $segments[0]);
-                   $segments = array_slice($segments, 1);
-
-                   // Does the requested controller exist in the root folder?
-                   if ((count($segments) > 0) && file_exists(APPPATH . 'controllers/' . $this->directory . $segments[0] . '.php')) {
-                       $subfolder = false;
-                   }
-               }
-               /*---END--Sub-folder--*//*
-
-
-   			if (count($segments) > 0)
-   			{
-   				// Does the requested controller exist in the sub-folder?
-   				if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$segments[0] . '.php'))
-   				{
-   					show_404($this->fetch_directory().$segments[0]);
-   				}
-   			}
-   			else
-   			{
-   				// Is the method being specified in the route?
-   				if (strpos($this->default_controller, '/') !== FALSE)
-   				{
-   					$x = explode('/', $this->default_controller);
-
-   					$this->set_class($x[0]);
-   					$this->set_method($x[1]);
-   				}
-   				else
-   				{
-   					$this->set_class($this->default_controller);
-   					$this->set_method('index');
-   				}
-
-   				// Does the default controller exist in the sub-folder?
-   				if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$this->default_controller . '.php'))
-   				{
-   					$this->directory = '';
-   					return array();
-   				}
-
-   			}
-
-   			return $segments;
-   		}
-
-
-   		// If we've gotten this far it means that the URI does not correlate to a valid
-   		// controller class.  We will now see if there is an override
-   		if ( ! empty($this->routes['404_override']))
-   		{
-   			$x = explode('/', $this->routes['404_override']);
-
-   			$this->set_class($x[0]);
-   			$this->set_method(isset($x[1]) ? $x[1] : 'index');
-
-   			return $x;
-   		}
-
-
-   		// Nothing else to do at this point but show a 404
-   		show_404($segments[0]);
-
-        return false;
-   	}
-*/
-
     // --------------------------------------------------------------------
 
     /**
@@ -302,6 +191,7 @@ class MY_Router extends CI_Router
         $module_routes[0] = array();
         $module_routes[1] = array();
         $module_routes[2] = array();
+        $module_routes[3] = array();
 
         $dir = APPPATH . 'controllers' . '/';
         if ($handle = opendir($dir)) {
@@ -309,12 +199,18 @@ class MY_Router extends CI_Router
             {
                 if(is_file($dir . $file) && substr($file, -4) == '.php'){
                     $file = str_replace('.php', '', $file);
+                    $file = strtolower($file);
                     $module_routes[0][$file . '(/.*)?'] = $file . '/index$1';
                 } elseif(is_dir($dir2 = $dir . $file . DS) && !preg_match('/[^a-z0-9\/\\\_\-]/i', $dir2)) {
                     if ($handle2 = opendir($dir2)) {
                         while (false !== ($file2 = readdir($handle2))) {
+                            if(!preg_match('/[^a-z0-9]/i', $file2)){
+                                $module_routes[3][$file2] = strtolower($file) . '/' . strtolower($file2) . '/' . strtolower($file2) . '/index$1';
+                            }
                             if(is_file($dir2 . $file2) && substr($file2, -4) == '.php'){
                                 $file2 = str_replace('.php', '', $file2);
+                                $file = strtolower($file);
+                                $file2 = strtolower($file2);
                                 $module_routes[1][$file . '/' . $file2 . '(/.*)?'] = $file . '/' . $file2 . '/index$1';
                             } elseif(is_dir($dir3 = $dir2 . $file2 . DS) && !preg_match('/[^a-z0-9\/\\\_\-]/i', $dir3)) {
                                 if ($handle3 = opendir($dir3)) {
@@ -338,6 +234,9 @@ class MY_Router extends CI_Router
 
                                             $val .= $file2 . '/' . $file3;
 
+                                            $key = strtolower($key);
+                                            $val = strtolower($val);
+
                                             $module_routes[2][$key . '(/.*)?'] = $val . '/index$1';
                                         }
                                     }
@@ -352,7 +251,7 @@ class MY_Router extends CI_Router
         }
         closedir($handle);
 
-        $module_routes = array_merge($module_routes[2], $module_routes[1], $module_routes[0], $this->routes);
+        $module_routes = array_merge($module_routes[3], $module_routes[2], $module_routes[1], $module_routes[0], $this->routes);
         krsort($module_routes);
 
         return $module_routes;
