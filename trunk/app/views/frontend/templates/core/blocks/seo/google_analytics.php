45f909e8291dd63c5be55818154c08c35ce02f0e
dbashyal <dbashyal@gmail.com>
2016-07-20 15:44:24 +1000

    replaced old ga with new Universal Analytics tracking code

--- trunk/app/views/frontend/templates/core/blocks/seo/google_analytics.php
+++ trunk/app/views/frontend/templates/core/blocks/seo/google_analytics.php
@@ -1,15 +1,11 @@
 <?php if (!defined('BASEPATH')) exit(__('No direct script access allowed')); ?>
-<script type="text/javascript">
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', '<?php echo $this->setting->google_analytics_id ?>']);
-    //_gaq.push(['_setDomainName', 'codefight.org']); @todo::make it dynamic
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    ga('create', '<?php echo $this->setting->google_analytics_id ?>', 'auto');
+    ga('send', 'pageview');
 
 </script>
