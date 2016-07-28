0aad656547aaf4cc1ef263d87d56054166add336
dbashyal <dbashyal@gmail.com>
2016-07-28 20:32:53 +1000

    bumped version after CI and other few updates

--- trunk/app/controllers/frontend/tools/Version.php
+++ trunk/app/controllers/frontend/tools/Version.php
@@ -57,7 +57,7 @@ class Version extends MY_Controller
 
     public function index()
     {
-        echo '2.5.0.0';
+        echo '2.5.0.5';
 
         $install = xss_clean(str_replace(base_url() . 'tools/version/', '', current_url()));
         if(!empty($install)){
@@ -71,6 +71,3 @@ class Version extends MY_Controller
         }
     }
 }
-
-/* End of file page.php */
-/* Location: ./system/application/controllers/blog/page.php */
